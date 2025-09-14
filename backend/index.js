31883f00c48feeab0a4fb38b85b61f3131d1673b
Aditya122221 <adityakumar122221@gmail.com>
2025-09-14 20:33:40 +0530

    cors origin

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,23 @@ const port = process.env.PORT || 3000
 
 app.use(express.json({ limit: "16kb" }))
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+const allowedOrigins = [
+  process.env.CORS_ORIGIN_LOCAL,
+  process.env.CORS_ORIGIN_GLOBAL
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookieParser())
